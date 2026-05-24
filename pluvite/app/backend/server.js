f7bd2343537d9b7e48b73350acb4a4c64d8ba02e
Quezia Queren Nunes Luiz <quezialuiz81@gmail.com>
2026-05-24 14:33:58 -0300

    Atualização de dados

--- pluvite/app/backend/server.js
+++ pluvite/app/backend/server.js
@@ -69,3 +69,103 @@ app.post("/cadastrar-cidadao", async (req, res) => {
 app.listen(3001, () => {
   console.log("Servidor rodando na porta 3001");
 });
+
+app.post("/cadastro-prefeitura", async (req, res) => {
+  try {
+
+    const {
+      id_servidor,
+      email,
+      senha,
+      cargo,
+      re,
+    } = req.body;
+
+    // criptografar senha
+    const senhaHash = await bcrypt.hash(senha, 10);
+
+    // inserir no Supabase
+    const { data, error } = await supabase
+      .from("prefeitura")
+      .insert([
+        {
+          id_servidor: id_servidor,
+          email: email,
+          senha: senhaHash,
+          cargo: cargo,
+          re: re,
+        },
+      ]);
+
+    if (error) {
+      console.log(error);
+
+      return res.status(400).json({
+        error: error.message,
+      });
+    }
+
+    res.status(201).json({
+      message: "Servidor cadastrado com sucesso!",
+      data,
+    });
+
+  } catch (err) {
+
+    console.log(err);
+
+    res.status(500).json({
+      error: "Erro interno do servidor",
+    });
+
+  }
+});
+
+//login  
+app.post("/login", async (req, res) => {
+
+  try {
+
+    const { email, senha } = req.body;
+
+    // procurar usuário
+    const { data, error } = await supabase
+      .from("cidadao")
+      .select("*")
+      .eq("id_servidor", id_servidor)
+      .single();
+
+    if (error || !data) {
+      return res.status(400).json({
+        error: "Usuário não encontrado",
+      });
+    }
+
+    // comparar senha
+    const senhaCorreta = await bcrypt.compare(
+      senha,
+      data.senha
+    );
+
+    if (!senhaCorreta) {
+      return res.status(400).json({
+        error: "Senha incorreta",
+      });
+    }
+
+    res.status(200).json({
+      message: "Login realizado com sucesso!",
+      usuario: data,
+    });
+
+  } catch (err) {
+
+    console.log(err);
+
+    res.status(500).json({
+      error: "Erro interno",
+    });
+
+  }
+
+});
--- pluvite/app/backend/supabase.js
+++ pluvite/app/backend/supabase.js
@@ -1,10 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl =
-  "supabaseurl";
+  "https://qhughmeaxbyupuglpvud.supabase.co";
 
 const supabaseKey =
-  "supabasekey";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFodWdobWVheGJ5dXB1Z2xwdnVkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3OTAyNzgzMCwiZXhwIjoyMDk0NjAzODMwfQ.LbYDKH9U7IfKfJEbo_RAd4xkQPWBHzoFKLD5ADzdVyc";
 
 export const supabase = createClient(
   supabaseUrl,
