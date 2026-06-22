5b986f2bab28075bf78d3dbf8491895b066fb35d
Ana Clara Santos Pereira <pereira.anaclara1303@gmail.com>
2026-06-22 09:38:46 -0300

    Delete pluvite/.next/dev/static/chunks directory

--- pluvite/.next/dev/static/chunks/[project]_pluvite_pages__error_07xvfw~._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_next_dist_compiled_0o6l_m6._.js","static/chunks/node_modules_next_dist_shared_lib_0~pg0mt._.js","static/chunks/node_modules_next_dist_client_0pe1dg-._.js","static/chunks/node_modules_next_dist_0k8zr~w._.js","static/chunks/0kaq_next_dist_pages__error_0l4jhdg.js","static/chunks/[next]_entry_page-loader_ts_0udo10g._.js","static/chunks/node_modules_react-dom_0bruynb._.js","static/chunks/node_modules_0lx093h._.js","static/chunks/[root-of-the-server]__0zf5c5g._.js"],
-    source: "entry"
-});
